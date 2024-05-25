4ddac6fd2d52cefb5f0d2e8ffc122020aa43c75c
christian higa <higachristian@gmail.com>
2024-05-25 17:33:52 -0300

    Added HelloViewApi

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'feywb)55rxg-^btplitp8=1#g&8zfehs@*rb5c8@2s3_9x-(u8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
